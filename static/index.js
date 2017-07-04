30b2f48d14b7fcf1e57c1ccdcb0bf85e00d3d63e
Daniel Tao <daniel.tao@gmail.com>
2017-07-03 23:50:47 -0500

    chart results of single movie

--- static/index.js
+++ static/index.js
@@ -2,15 +2,44 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search-button').addEventListener('click', function(e) {
         e.preventDefault();
 
-        var resultsEl = document.getElementById('results');
-        resultsEl.textContent = '';
+        document.getElementById('results').innerHTML = '';
 
+        searchMovie(document.querySelector('input[name="title"]').value);
+    });
+
+    function searchMovie(title) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/search?title=' + encodeURIComponent(document.querySelector('input[name="title"]').value));
+        xhr.open('GET', '/search?title=' + encodeURIComponent(movieTitle);
         xhr.addEventListener('load', function() {
-            var result = JSON.parse(xhr.responseText);
-            resultsEl.textContent = JSON.stringify(result, null, 2);
+            chartMovie(JSON.parse(xhr.responseText));
         });
         xhr.send();
-    });
+    }
+
+    function chartMovie(data) {
+        Highcharts.chart('results', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: data.title + ' Box Office'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                type: 'line',
+                name: 'Daily Gross',
+                data: data.box_office.map(function(daily) {
+                    return [Date.parse(daily.date), daily.gross];
+                })
+            }],
+            credits: {
+                enabled: false
+            }
+        });
+    }
 });
