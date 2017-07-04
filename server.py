1fdf74ca2604e629919c8699aa69fa793d667d49
Daniel Tao <daniel.tao@gmail.com>
2017-07-03 21:34:51 -0500

    simple bottle server

---
+++ server.py
@@ -0,0 +1,21 @@
+import os
+
+from bottle import request, route, run
+
+from pybomojo import get_box_office, get_movie_id
+
+HOST = os.getenv('HOST', 'localhost')
+PORT = int(os.getenv('PORT', 8080))
+
+
+@route('/')
+def index():
+    search_term = request.query.q
+
+    movie_id = get_movie_id(search_term)
+    if movie_id is None:
+        return None
+
+    return get_box_office(movie_id)
+
+run(host=HOST, port=PORT)
