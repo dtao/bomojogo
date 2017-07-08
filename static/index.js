332644fb062c92bf1ddf14546c3a0618a11d78de
Daniel Tao <daniel.tao@gmail.com>
2017-07-07 19:35:29 -0500

    refactor to reduce duplication

--- static/index.js
+++ static/index.js
@@ -36,91 +36,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function chartMovies(results, maxResults) {
-        Highcharts.chart('daily-results', {
-            chart: {
-                zoomType: 'x'
-            },
-            title: {
-                text: 'Daily Box Office'
-            },
-            xAxis: {
-                title: {
-                    'text': 'Days in release'
-                },
-                type: 'linear'
-            },
-            yAxis: {
-                title: {
-                    text: 'Daily box office'
-                }
-            },
-            plotOptions: {
-                series: {
-                    pointStart: 1
-                }
-            },
-            series: results.map(function(result) {
-                var boxOffice = result.box_office;
+        Highcharts.chart('daily-results', getChartOptions(
+            'Daily box office', results, 'gross', maxResults));
 
-                if (maxResults) {
-                    boxOffice = boxOffice.slice(0, maxResults);
-                }
-
-                return {
-                    type: 'line',
-                    name: result.title,
-                    data: boxOffice.map(function(daily) {
-                        return [daily.day, daily.gross];
-                    })
-                };
-            }),
-            credits: {
-                enabled: false
-            }
-        });
-
-        Highcharts.chart('cumulative-results', {
-            chart: {
-                zoomType: 'x'
-            },
-            title: {
-                text: 'Cumulative Box Office'
-            },
-            xAxis: {
-                title: {
-                    'text': 'Days in release'
-                },
-                type: 'linear'
-            },
-            yAxis: {
-                title: {
-                    text: 'Cumulative box office'
-                }
-            },
-            plotOptions: {
-                series: {
-                    pointStart: 1
-                }
-            },
-            series: results.map(function(result) {
-                var boxOffice = result.box_office;
-
-                if (maxResults) {
-                    boxOffice = boxOffice.slice(0, maxResults);
-                }
-
-                return {
-                    type: 'line',
-                    name: result.title,
-                    data: boxOffice.map(function(daily) {
-                        return [daily.day, daily.cumulative];
-                    })
-                };
-            }),
-            credits: {
-                enabled: false
-            }
-        });
+        Highcharts.chart('cumulative-results', getChartOptions(
+            'Cumulative box office', results, 'cumulative', maxResults));
     }
 
     function getMaxResults(period) {
@@ -139,5 +59,50 @@ document.addEventListener('DOMContentLoaded', function() {
             default:
                 return value;
         }
+    }
+
+    function getChartOptions(title, results, property, maxResults) {
+        return {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: title
+            },
+            xAxis: {
+                title: {
+                    'text': 'Days in release'
+                },
+                type: 'linear'
+            },
+            yAxis: {
+                title: {
+                    text: title
+                }
+            },
+            plotOptions: {
+                series: {
+                    pointStart: 1
+                }
+            },
+            series: results.map(function(result) {
+                var boxOffice = result.box_office;
+
+                if (maxResults) {
+                    boxOffice = boxOffice.slice(0, maxResults);
+                }
+
+                return {
+                    type: 'line',
+                    name: result.title,
+                    data: boxOffice.map(function(daily) {
+                        return [daily.day, daily[property]];
+                    })
+                };
+            }),
+            credits: {
+                enabled: false
+            }
+        };
     }
 });
