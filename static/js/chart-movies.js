1f1d6e3483acebd0d73cd3105322265434c4ea1d
Daniel Tao <daniel.tao@gmail.com>
2017-08-06 17:47:28 -0700

    extract chartMovies into its own module

---
+++ static/js/chart-movies.js
@@ -0,0 +1,117 @@
+function chartMovies(containerId, results, property, title, maxResults) {
+    Highcharts.chart(containerId,
+      getChartOptions(title, results, property, maxResults));
+}
+
+function getChartOptions(title, results, property, maxResults) {
+    return {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: title
+        },
+        xAxis: {
+            title: {
+                'text': 'Weeks in release'
+            },
+            labels: {
+                formatter: function() {
+                    var week = Math.floor(this.value / 7) + 1;
+                    return formatFriday(week);
+                }
+            },
+            tickPositioner: function() {
+                var positions = [];
+
+                var largestResult = results.reduce(function(a, b) {
+                    return a.box_office.length > b.box_office.length ? a : b;
+                }, results[0]);
+
+                largestResult.box_office.forEach(function(daily, i) {
+                    if (daily.day == 'Fri') {
+                        positions.push(i + 1);
+                    }
+                });
+
+                // Don't display *too* many data labels (e.g. when using
+                // "Forever" as the time period).
+                if (positions.length > 10) {
+                    positions = positions.filter(function(pos, i) {
+                        return i % 2 == 0;
+                    });
+                }
+
+                return positions;
+            }
+        },
+        yAxis: {
+            title: {
+                text: title
+            }
+        },
+        plotOptions: {
+            line: {
+                marker: {
+                    enabled: false
+                }
+            },
+            series: {
+                pointStart: 1
+            }
+        },
+        legend: {
+            labelFormatter: function() {
+                return this.name + ' <a href="' + results[this.index].href +
+                    '" target="_blank"><small><span class="glyphicon ' +
+                    'glyphicon-new-window"></span></small></a>';
+            },
+            useHTML: true
+        },
+        series: results.map(function(result) {
+            var boxOffice = result.box_office;
+
+            if (maxResults) {
+                boxOffice = boxOffice.slice(0, maxResults);
+            }
+
+            return {
+                type: 'line',
+                name: result.title,
+                data: boxOffice.map(function(daily) {
+                    return [daily.day, daily[property]];
+                })
+            };
+        }),
+        credits: {
+            enabled: false
+        }
+    };
+}
+
+function formatFriday(week) {
+    var suffix;
+    switch (week % 10) {
+        case 1:
+            suffix = 'st';
+            break;
+        case 2:
+            if (week % 100 != 12) {
+                suffix = 'nd';
+            }
+            break;
+        case 3:
+            if (week % 100 != 13) {
+                suffix = 'rd';
+            }
+            break;
+    }
+
+    if (!suffix) {
+        suffix = 'th';
+    }
+
+    return week + suffix + ' Friday';
+}
+
+module.exports = chartMovies;
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+import chartMovies from './chart-movies.js';
+
 import '../css/index.css';
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -87,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function displayState(state) {
         populateForm(state.movies, state.period);
-        chartMovies(state.results, getMaxResults(state.period));
+        renderCharts(state.results, getMaxResults(state.period));
         updateTitle(state.movies);
     }
 
@@ -111,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (results.length == movies.length) {
                     extractErrors(results);
                     dayOffset = alignResults(results);
-                    chartMovies(results, maxResults + dayOffset);
+                    renderCharts(results, maxResults + dayOffset);
                     history.pushState({
                         'movies': movies,
                         'period': period,
@@ -210,12 +212,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function chartMovies(results, maxResults) {
-        Highcharts.chart('daily-results', getChartOptions(
-            'Daily box office', results, 'gross', maxResults));
-
-        Highcharts.chart('cumulative-results', getChartOptions(
-            'Cumulative box office', results, 'cumulative', maxResults));
+    function renderCharts(results, maxResults) {
+        chartMovies('daily-results', results, 'gross', 'Daily box office',
+            maxResults);
+        chartMovies('cumulative-results', results, 'cumulative',
+            'Cumulative box office', maxResults);
     }
 
     function populateForm(movies, period) {
@@ -294,92 +295,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function getChartOptions(title, results, property, maxResults) {
-        return {
-            chart: {
-                zoomType: 'x'
-            },
-            title: {
-                text: title
-            },
-            xAxis: {
-                title: {
-                    'text': 'Weeks in release'
-                },
-                labels: {
-                    formatter: function() {
-                        var week = Math.floor(this.value / 7) + 1;
-                        return formatFriday(week);
-                    }
-                },
-                tickPositioner: function() {
-                    var positions = [];
-
-                    var largestResult = results.reduce(function(a, b) {
-                        return a.box_office.length > b.box_office.length ? a : b;
-                    }, results[0]);
-
-                    largestResult.box_office.forEach(function(daily, i) {
-                        if (daily.day == 'Fri') {
-                            positions.push(i + 1);
-                        }
-                    });
-
-                    // Don't display *too* many data labels (e.g. when using
-                    // "Forever" as the time period).
-                    if (positions.length > 10) {
-                        positions = positions.filter(function(pos, i) {
-                            return i % 2 == 0;
-                        });
-                    }
-
-                    return positions;
-                }
-            },
-            yAxis: {
-                title: {
-                    text: title
-                }
-            },
-            plotOptions: {
-                line: {
-                    marker: {
-                        enabled: false
-                    }
-                },
-                series: {
-                    pointStart: 1
-                }
-            },
-            legend: {
-                labelFormatter: function() {
-                    return this.name + ' <a href="' + results[this.index].href +
-                        '" target="_blank"><small><span class="glyphicon ' +
-                        'glyphicon-new-window"></span></small></a>';
-                },
-                useHTML: true
-            },
-            series: results.map(function(result) {
-                var boxOffice = result.box_office;
-
-                if (maxResults) {
-                    boxOffice = boxOffice.slice(0, maxResults);
-                }
-
-                return {
-                    type: 'line',
-                    name: result.title,
-                    data: boxOffice.map(function(daily) {
-                        return [daily.day, daily[property]];
-                    })
-                };
-            }),
-            credits: {
-                enabled: false
-            }
-        };
-    }
-
     function getDayAtOrdinal(results, ordinal) {
         for (var i = 0; i < results.length; ++i) {
             if (ordinal > 0 && ordinal <= results[i].box_office.length) {
@@ -388,30 +303,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         return null;
-    }
-
-    function formatFriday(week) {
-        var suffix;
-        switch (week % 10) {
-            case 1:
-                suffix = 'st';
-                break;
-            case 2:
-                if (week % 100 != 12) {
-                    suffix = 'nd';
-                }
-                break;
-            case 3:
-                if (week % 100 != 13) {
-                    suffix = 'rd';
-                }
-                break;
-        }
-
-        if (!suffix) {
-            suffix = 'th';
-        }
-
-        return week + suffix + ' Friday';
     }
 });
