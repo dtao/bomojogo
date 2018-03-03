c39b7992b7ce13899fe4e37f6a4e0d219634430a
Daniel Tao <daniel.tao@gmail.com>
2018-03-03 13:51:58 -0600

    remove loading spinner when rendering errors

--- static/js/index.js
+++ static/js/index.js
@@ -111,12 +111,13 @@ document.addEventListener('DOMContentLoaded', function() {
         // Load one known good movie to ensure the API is awake.
         document.body.classList.add('loading');
         getBoxOffice({ title: 'Wonder Woman', movie_id: 'wonderwoman.htm' }, function(result) {
+            document.body.classList.remove('loading');
+
             if (result.error) {
                 displayErrors(["Oh no! The API isn't responding :("]);
                 return;
             }
 
-            document.body.classList.remove('loading');
             document.querySelector('#search-form fieldset').removeAttribute('disabled');
         });
 
